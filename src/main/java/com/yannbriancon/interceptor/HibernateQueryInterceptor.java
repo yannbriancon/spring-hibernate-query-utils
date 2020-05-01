10c1dbd50087bb50cd2f0011cc63ff40072bd684
Yann Briancon <briyan@sfr.fr>
2020-05-01 17:48:31 +0200

    Add N+1 query detection

--- src/main/java/com/yannbriancon/interceptor/HibernateQueryInterceptor.java
+++ src/main/java/com/yannbriancon/interceptor/HibernateQueryInterceptor.java
@@ -1,13 +1,24 @@
 package com.yannbriancon.interceptor;
 
+import com.yannbriancon.exception.NPlusOneQueryException;
 import org.hibernate.EmptyInterceptor;
+import org.hibernate.Transaction;
+import org.hibernate.proxy.HibernateProxy;
 import org.springframework.stereotype.Component;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 
 @Component
 public class HibernateQueryInterceptor extends EmptyInterceptor {
 
     private transient ThreadLocal<Long> threadQueryCount = new ThreadLocal<>();
 
+    private transient ThreadLocal<Set<String>> threadPreviouslyLoadedEntities = new ThreadLocal<>();
+
+    public HibernateQueryInterceptor() {
+        threadPreviouslyLoadedEntities.set(new HashSet<>());
     }
 
     /**
@@ -37,5 +48,41 @@ public class HibernateQueryInterceptor extends EmptyInterceptor {
             threadQueryCount.set(count + 1);
         }
         return super.onPrepareStatement(sql);
+    }
+
+    /**
+     * Reset previously loaded entities after the end of a transaction to avoid triggering
+     * N+1 query exceptions because of loading same instance in two different transactions
+     *
+     * @param tx Transaction having been completed
+     */
+    @Override
+    public void afterTransactionCompletion(Transaction tx) {
+        threadPreviouslyLoadedEntities.set(new HashSet<>());
+    }
+
+    /**
+     * Detect the N+1 queries by checking if two calls were made to getEntity for the same instance
+     * <p>
+     * The first call is made with the instance filled with a {@link HibernateProxy}
+     * and the second is made after a query was executed to fetch the data in the Entity
+     *
+     * @param entityName Name of the entity to get
+     * @param id         Id of the entity to get
+     */
+    @Override
+    public Object getEntity(String entityName, Serializable id) {
+        Set<String> previouslyLoadedEntities = threadPreviouslyLoadedEntities.get();
+
+        if (previouslyLoadedEntities.contains(entityName + id)) {
+            previouslyLoadedEntities.remove(entityName + id);
+            threadPreviouslyLoadedEntities.set(previouslyLoadedEntities);
+            throw new NPlusOneQueryException("N+1 query detected for entity: " + entityName);
+        }
+
+        previouslyLoadedEntities.add(entityName + id);
+        threadPreviouslyLoadedEntities.set(previouslyLoadedEntities);
+
+        return null;
     }
 }
