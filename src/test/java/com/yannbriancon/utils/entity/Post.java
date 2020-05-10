c150436bbefd9204299d74a4d76b58627d53660b
Yann Briancon <briyan@sfr.fr>
2020-05-10 10:36:45 +0200

    Add Posts to test the missing lazy configuration

---
+++ src/test/java/com/yannbriancon/utils/entity/Post.java
@@ -0,0 +1,41 @@
+package com.yannbriancon.utils.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "posts")
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "message_id")
+    private Message message;
+
+    public Post() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Message getMessage() {
+        return message;
+    }
+
+    public void setMessage(Message message) {
+        this.message = message;
+    }
+}
--- src/test/resources/import.sql
+++ src/test/resources/import.sql
@@ -5,3 +5,7 @@ VALUES (1, 'name'),
 INSERT INTO messages(id, text, author_id)
 VALUES (1, 'small desc', 1),
        (2, 'long desc', 2);
+
+INSERT INTO posts(id, message_id)
+VALUES (1, 1),
+       (2, 2);
