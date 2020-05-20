ab8e5a4606f15f2c74ccacefa9eedac316c2c749
Eugene Kennedy <eugene.kennedy@betterment.com>
2020-05-20 16:25:41 -0400

    Add enabled property for N+1 detection

---
+++ src/test/java/com/yannbriancon/interceptor/NPlusOneQueriesDisabledTest.java
@@ -0,0 +1,44 @@
+package com.yannbriancon.interceptor;
+
+import com.yannbriancon.utils.entity.Message;
+import com.yannbriancon.utils.repository.MessageRepository;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(properties = { "hibernate.query.interceptor.error-level=EXCEPTION", "hibernate.query.interceptor.n-plus-one-detection-enabled=false" })
+@Transactional
+class NPlusOneQueriesDisabledTest {
+
+    @Autowired
+    private MessageRepository messageRepository;
+
+    @Autowired
+    private HibernateQueryInterceptor hibernateQueryInterceptor;
+
+    @Test
+    void nPlusOneQueriesDetection_whenDisabled_stillCountsQueries_doesNotThrowException() {
+        // Fetch the 2 messages without the authors
+        List<Message> messages = messageRepository.findAll();
+
+        hibernateQueryInterceptor.startQueryCount();
+
+        // Trigger N+1 queries, if this WAS enabled, it would throw an exception and fail the test.
+        messages.stream()
+            .map(message -> message.getAuthor().getName())
+            .collect(Collectors.toList());
+
+        //Assert that counting still runs when n+1 detection is disabled
+        assertThat(hibernateQueryInterceptor.getQueryCount()).isEqualTo(2);
+    }
+}
