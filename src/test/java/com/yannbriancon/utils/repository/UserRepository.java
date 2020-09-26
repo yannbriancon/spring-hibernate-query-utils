eb8cdf2ef2e5245d57077449bde6b1668031a74c
Yann Briancon <briyan@sfr.fr>
2020-09-26 19:30:36 +0200

    Component to Repository

--- src/test/java/com/yannbriancon/utils/repository/UserRepository.java
+++ src/test/java/com/yannbriancon/utils/repository/UserRepository.java
@@ -2,8 +2,8 @@ package com.yannbriancon.utils.repository;
 
 import com.yannbriancon.utils.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
-@Component
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
